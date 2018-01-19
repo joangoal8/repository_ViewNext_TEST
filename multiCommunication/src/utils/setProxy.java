91ab4f162295f10fd4d697ec8a718e979b15be7c
Joan Gomez Alvarez <joangoal8@gmail.com>
2018-01-19 08:59:56 +0100

    S'ha intentat canviar la configuració del proxy per codi

---
+++ multiCommunication/src/utils/setProxy.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class setProxy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*System.out.println("SETTING.....");
+		
+		System.setProperty("http.proxyHost", "10.115.4.2");
+		System.setProperty("http.proxyPort", "8080");
+		
+		System.out.println("FINISH SETTING");*/
+	}
+
+}
